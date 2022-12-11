2addbafe012c2233a03a5806959a6c96af2a3add
Preet Sojitra <preet.dev373@gmail.com>
2022-12-11 18:09:53 +0530

    success page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import ChooseLab from "./pages/ChooseLab"
 import HomePage from "./pages/HomePage"
 import Summary from "./pages/Summary"
+import Success from "./pages/Success"
 
 function App() {
   return (
     <div className="font-Roboto bg-background">
       {/* <HomePage /> */}
-      <Summary />
+      {/* <Summary /> */}
+      <Success/>
     </div>
   )
 }
