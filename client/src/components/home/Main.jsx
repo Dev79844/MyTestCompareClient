89923eb451ef28c03eba43276b3058ea6a325f61
Preet Sojitra <preet.dev373@gmail.com>
2022-12-24 22:50:46 +0530

    feat: add city and lab select dropdown

--- client/src/components/home/Main.jsx
+++ client/src/components/home/Main.jsx
@@ -1,13 +1,47 @@
 import React from "react"
 import {Icon} from "@iconify/react"
 import "../../style.css"
-import { Link } from "react-router-dom"
+import {Link} from "react-router-dom"
 import Success from "../../pages/Success"
 
-export default function Main() {
+import Select from "react-select"
 
-  const location = navigator.geolocation.getCurrentPosition(Success,Error)
+export default function Main() {
+  const location = navigator.geolocation.getCurrentPosition(Success, Error)
   console.log(location)
+
+  const cityNames = [
+    {value: "New York", label: "New York"},
+    {value: "Los Angeles", label: "Los Angeles"},
+    {value: "Chicago", label: "Chicago"},
+    {value: "Houston", label: "Houston"},
+    {value: "Philadelphia", label: "Philadelphia"},
+    {value: "Phoenix", label: "Phoenix"},
+    {value: "San Antonio", label: "San Antonio"},
+    {value: "San Diego", label: "San Diego"},
+    {value: "Dallas", label: "Dallas"},
+    {value: "San Jose", label: "San Jose"},
+  ]
+
+  const testNames = [
+    {value: "Test 1", label: "Test 1"},
+    {value: "Test 2", label: "Test 2"},
+    {value: "Test 3", label: "Test 3"},
+    {value: "Test 4", label: "Test 4"},
+    {value: "Test 5", label: "Test 5"},
+  ]
+
+  const selctStyles = {
+    control: (baseStyles) => ({
+      ...baseStyles,
+      backgroundColor: "transparent",
+      border: "none",
+      fontFamily: "Roboto",
+      fontSize: "18px",
+      fontWeight: "500",
+      cursor: "pointer",
+    }),
+  }
 
   const credebility = [
     {
@@ -17,7 +51,7 @@ export default function Main() {
     },
     {
       id: 1,
-      icon: "material-symbols:more-time",
+      icon: "ic:outline-access-time",
       h3: "OnTime Reports",
     },
     {
@@ -30,10 +64,14 @@ export default function Main() {
 
   const credebilityArr = credebility.map((item) => {
     return (
-      <div key={item.id} className={`flex items-center gap-2 mb-5 sm:gap-2 lg:mb-0 ${item.h3 == "OnTime Reports" ? "hidden sm:flex" : ""} `} >
+      <div
+        key={item.id}
+        className={`flex items-center gap-2 mb-5 sm:gap-2 lg:mb-0 ${
+          item.h3 == "OnTime Reports" ? "hidden sm:flex" : ""
+        } `}
+      >
         <div className="bg-background rounded-full p-[6px]">
-
-        <Icon icon={item.icon} color="#000000" className="text-3xl" />
+          <Icon icon={item.icon} color="#000000" className="text-3xl" />
         </div>
         <div className="text-lg sm:text-xl md:text-2xl">
           <h3 className="font-medium ">{item.h3}</h3>
@@ -44,60 +82,76 @@ export default function Main() {
   })
 
   return (
-    <div className=" mainBg  pt-8 lg:relative"> 
+    <div className=" mainBg  pt-8 lg:relative">
       <div className="hidden lg:block lg:absolute lg:w-36 lg:right-16 lg:top-24">
-        <img src="images/home/bestPrice.png" alt="Our Promise, Best Price"/>
+        <img src="images/home/bestPrice.png" alt="Our Promise, Best Price" />
       </div>
       <h1 className="text-white font-bold text-4xl w-[70%]  m-auto text-center lg:text-6xl lg:w-full ">
         Book Any Test at Best Price
       </h1>
       <div className="bg-primary p-4 my-6 rounded-md w-[90%] mx-auto xs:w-4/5 sm:w-3/5 sm:px-8 lg:w-[38%] 2xl:w-[30%]">
         {/* Location input */}
-        <div className="flex bg-background py-2 px-5 items-center rounded-md my-3">
+        <div className="flex bg-background py-2 px-5 gap-2 items-center rounded-md my-3">
           <Icon
             icon="material-symbols:my-location-rounded"
             color="#22242e"
             className="opacity-50 text-2xl"
           />
-          <input
+          {/* <input
             type="text"
             className="pl-3 w-full text-lg bg-transparent"
-            placeholder={location}
+            placeholder="Enter Location"
+          /> */}
+          <Select
+            options={cityNames}
+            styles={selctStyles}
+            className="w-full"
+            placeholder="Select City"
+            isMulti={false}
           />
         </div>
 
         {/* Test input */}
-        <div className="flex bg-background py-2 px-5 items-center rounded-md my-3">
+        <div className="flex bg-background gap-2 py-2 px-5 items-center rounded-md my-3">
           <Icon
             icon="mdi:test-tube"
             color="#22242e"
             className="opacity-50 text-2xl"
           />
-          <input
+          {/* <input
             type="text"
             className="pl-3 w-full text-lg bg-transparent"
             placeholder="Select Tests"
+          /> */}
+          <Select
+            options={testNames}
+            styles={selctStyles}
+            className="w-full"
+            placeholder="Select Tests"
+            isMulti={true}
           />
         </div>
 
         {/* Book Now */}
         <Link to="/chooseLab">
           <div className="flex justify-center">
-              <button className="bg-secondary text-white font-bold flex justify-center items-center py-1 px-4 rounded-md w-3/5 lg:w-1/2 text-xl  group">
-                Book Now
-                <Icon
-                  icon="material-symbols:arrow-right-alt"
-                  color="white"
-                  className="text-2xl ml-3 group-hover:translate-x-1 transition-all duration-300 ease-in-out"
-                />
-              </button>
+            <button className="bg-secondary text-white font-bold flex justify-center items-center py-1 px-4 rounded-md w-3/5 lg:w-1/2 text-xl  group">
+              Book Now
+              <Icon
+                icon="material-symbols:arrow-right-alt"
+                color="white"
+                className="text-2xl ml-3 group-hover:translate-x-1 transition-all duration-300 ease-in-out"
+              />
+            </button>
           </div>
         </Link>
       </div>
 
       {/* Credibility */}
       <div className="bg-primary mt-12 lg:mt-8">
-        <div className="pt-4 pb-1 flex mx-2 lg:mx-0 justify-evenly items-center gap-1 sm:gap-7 lg:justify-evenly lg:w-full lg:pt-2">{credebilityArr}</div>
+        <div className="pt-4 pb-1 flex mx-2 lg:mx-0 justify-evenly items-center gap-1 sm:gap-7 lg:justify-evenly lg:w-full lg:pt-2">
+          {credebilityArr}
+        </div>
       </div>
     </div>
   )
