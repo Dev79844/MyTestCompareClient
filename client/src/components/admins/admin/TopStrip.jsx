1473880a754bf197a2c2814d459087e6931b3973
Preet Sojitra <preet.dev373@gmail.com>
2022-12-28 01:45:52 +0530

    feat: add fetching of lab name from backend and show on profile

--- client/src/components/admins/admin/TopStrip.jsx
+++ client/src/components/admins/admin/TopStrip.jsx
@@ -1,12 +1,38 @@
 import {Icon} from "@iconify/react"
 import React from "react"
+import axios from "axios"
 
 export default function TopStrip() {
+  const [labName, setLabName] = React.useState("")
+  const [loading, setLoading] = React.useState(true)
+
+  axios
+    .get("http://localhost:3000/api/v1/manage/lab", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
+      },
+    })
+    .then((res) => {
+      setLabName(res.data.data.lab.name)
+      setLoading(false)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
   return (
     <div>
       <div className="flex gap-2 items-center justify-end">
         <Icon icon={"ion:notifcations"} color="black" className="text-3xl" />
-        <h2 className="font-medium  text-2xl"> Anushka Pathology Laboratory</h2>
+        <h2 className="font-medium  text-2xl"> {labName}</h2>
       </div>
     </div>
   )
