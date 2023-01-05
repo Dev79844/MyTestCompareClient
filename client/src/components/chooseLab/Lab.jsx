77b4f05bae6aac93534e28fdadc7a73f9999e897
Preet Sojitra <preet.dev373@gmail.com>
2023-01-05 14:12:59 +0530

    fix: chooseLab issue

--- client/src/components/chooseLab/Lab.jsx
+++ client/src/components/chooseLab/Lab.jsx
@@ -20,7 +20,6 @@ export default function Lab() {
   const {city, tests} = queryParams
 
   const [labArr, setLabArr] = React.useState([])
-  const [labFound, setLabFound] = React.useState(false)
 
   React.useEffect(() => {
     getLabs()
@@ -40,20 +39,10 @@ export default function Lab() {
     setLabArr(data.data.data.labs)
   }
 
-  // console.log(labArr)
-
-  let maxProcessingTime
-  let processingTimeA
-  if (labArr.length === 0 && !labFound) {
-    return (
-      <div className="flex flex-col items-center justify-center h-[80vh]">
-        <p className="text-2xl font-bold">No Labs Found</p>
-        <p className="text-lg font-medium text-gray-500">
-          Please try again with different tests
-        </p>
-      </div>
-    )
-  } else {
+  console.log(labArr.length)
+  if (labArr.length != 0) {
+    let maxProcessingTime
+    let processingTimeA
     labArr.forEach((lab) => {
       const processingTimeArr = []
       lab.tests.forEach((test) => {
@@ -65,176 +54,176 @@ export default function Lab() {
       })
       maxProcessingTime = Math.max(...processingTimeArr)
     })
-  }
-  console.log(labArr)
+    console.log(labArr)
 
-  const labs = labArr.map((lab, index) => {
-    return (
-      <div
-        key={lab._id}
-        className={`flex justify-between gap-4 py-3 px-4 sm:grid sm:grid-cols-chooseLab
+    const labs = labArr.map((lab, index) => {
+      return (
+        <div
+          key={lab._id}
+          className={`flex justify-between gap-4 py-3 px-4 sm:grid sm:grid-cols-chooseLab
       ${index % 2 != 0 ? "bg-secondary-withOpacity" : "bg-transparent"}
         `}
-      >
-        <div>
-          <p className="font-medium">{lab.name}</p>
-          <div className="flex gap-[3rem] text-[0.85rem]">
-            {/* <div className="flex items-center gap-1">
+        >
+          <div>
+            <p className="font-medium">{lab.name}</p>
+            <div className="flex gap-[3rem] text-[0.85rem]">
+              {/* <div className="flex items-center gap-1">
               <Icon icon={"mdi:badge-outline"} color={"#E97F0E"} />
               {lab.certificate}
             </div> */}
-            {/* <div className="flex items-center gap-1">
+              {/* <div className="flex items-center gap-1">
               <Icon icon={"simple-line-icons:calender"} color={"#E97F0E"} />
               <span className="font-bold">Timings:</span> {lab.timings}
             </div> */}
-          </div>
-          <div className="flex items-center gap-1">
-            <Icon
-              icon={"material-symbols:location-on-outline"}
-              color={"#E97F0E"}
-            />
-            <p className="text-[0.85rem]">
-              {lab.address.address} , {lab.address.city} - {lab.address.zipcode}{" "}
-            </p>
-          </div>
-          <div className="flex items-center gap-1">
-            <Icon icon={"mdi:information-outline"} color={"#E97F0E"} />
-            <p className="text-[0.85rem]">
-              Processing time: {`${maxProcessingTime} hours`}
-            </p>
-          </div>
-          {/* <div className="flex items-center gap-1">
+            </div>
+            <div className="flex items-center gap-1">
+              <Icon
+                icon={"material-symbols:location-on-outline"}
+                color={"#E97F0E"}
+              />
+              <p className="text-[0.85rem]">
+                {lab.address.address} , {lab.address.city} -{" "}
+                {lab.address.zipcode}{" "}
+              </p>
+            </div>
+            <div className="flex items-center gap-1">
+              <Icon icon={"mdi:information-outline"} color={"#E97F0E"} />
+              <p className="text-[0.85rem]">
+                Processing time: {`${maxProcessingTime} hours`}
+              </p>
+            </div>
+            {/* <div className="flex items-center gap-1">
             <Icon icon={"game-icons:path-distance"} color={"#E97F0E"} />
             <p className="text-[0.85rem]">Distance: {lab.distance}</p>
           </div> */}
-          {/* <Link to="/afterLab"> */}
-          <button
-            className="text-white  bg-primary rounded  px-4 py-1 mt-2 sm:hidden"
-            onClick={() => {
-              const queryParams = {
-                lab: lab.name,
-                tests: tests,
-              }
-              const queryString = qs.stringify(queryParams)
-              navigate({
-                pathname: "/afterLab",
-                search: `?${queryString}`,
-              })
-            }}
-          >
-            Select Lab
-          </button>
-          {/* </Link> */}
+            {/* <Link to="/afterLab"> */}
+            <button
+              className="text-white  bg-primary rounded  px-4 py-1 mt-2 sm:hidden"
+              onClick={() => {
+                const queryParams = {
+                  lab: lab.name,
+                  tests: tests,
+                }
+                const queryString = qs.stringify(queryParams)
+                navigate({
+                  pathname: "/afterLab",
+                  search: `?${queryString}`,
+                })
+              }}
+            >
+              Select Lab
+            </button>
+            {/* </Link> */}
+          </div>
+          <div className="pt-[2.5rem] sm:pt-0 sm:self-center">
+            <p>
+              {" "}
+              ₹
+              {lab.tests.reduce((acc, curr) => {
+                return acc + curr.price
+              }, 0)}
+            </p>
+          </div>
+          <div className=" hidden sm:block sm:self-center">
+            {/* <Link to="/afterLab"> */}
+            <button
+              className="text-white  bg-primary rounded  px-4 py-1 mt-2 "
+              onClick={() => {
+                const queryParams = {
+                  lab: lab.name,
+                  tests: tests,
+                }
+                const queryString = qs.stringify(queryParams)
+                navigate({
+                  pathname: "/afterLab",
+                  search: `?${queryString}`,
+                })
+              }}
+            >
+              Select Lab
+            </button>
+            {/* </Link> */}
+          </div>
         </div>
-        <div className="pt-[2.5rem] sm:pt-0 sm:self-center">
-          <p>
-            {" "}
+      )
+    })
+
+    const largeLabs = labArr.map((lab, index) => {
+      return (
+        <div
+          key={lab._id}
+          className={`grid grid-cols-chooseLab5 p-3  ${
+            index % 2 != 0 ? "bg-secondary-withOpacity" : "bg-transparent "
+          }
+      `}
+        >
+          <div className="grid grid-cols-chooseLab2">
+            <p className="font-medium col-span-2 text-xl">{lab.name}</p>
+            <div className="flex gap-[3rem] text-[0.85rem] col-span-2 my-1">
+              {/* <div className="flex items-center gap-1">
+              <Icon icon={"mdi:badge-outline"} color={"#E97F0E"} />
+              {lab.basic.certificate}
+            </div> */}
+              {/* <div className="flex items-center gap-1">
+              <Icon icon={"simple-line-icons:calender"} color={"#E97F0E"} />
+              <span className="font-bold">Timings:</span> {lab.basic.timings}
+            </div> */}
+            </div>
+            <div className="flex items-center gap-1 col-span-2">
+              <Icon
+                icon={"material-symbols:location-on-outline"}
+                color={"#E97F0E"}
+              />
+              <p className="text-[0.85rem] ">
+                {lab.address.address}, {lab.address.city} -{" "}
+                {lab.address.zipcode}
+              </p>
+            </div>
+          </div>
+          <h1 className="justify-self-center self-center">{`${maxProcessingTime} hours`}</h1>
+          {/* <h1 className="justify-self-center self-center">{lab.distance} km</h1> */}
+          <h1 className="justify-self-center self-center text-xl">
             ₹
             {lab.tests.reduce((acc, curr) => {
               return acc + curr.price
             }, 0)}
-          </p>
+          </h1>
+          <div className="self-center justify-self-center">
+            {/* <Link to="/afterLab"> */}
+            <button
+              className="text-white  bg-primary rounded  px-4 py-1 mt-2"
+              onClick={() => {
+                const queryParams = {
+                  lab: lab.name,
+                  tests: tests,
+                }
+                const queryString = qs.stringify(queryParams)
+                navigate({
+                  pathname: "/afterLab",
+                  search: `?${queryString}`,
+                })
+              }}
+            >
+              Select Lab
+            </button>
+            {/* </Link> */}
+          </div>
         </div>
-        <div className=" hidden sm:block sm:self-center">
-          {/* <Link to="/afterLab"> */}
-          <button
-            className="text-white  bg-primary rounded  px-4 py-1 mt-2 "
-            onClick={() => {
-              const queryParams = {
-                lab: lab.name,
-                tests: tests,
-              }
-              const queryString = qs.stringify(queryParams)
-              navigate({
-                pathname: "/afterLab",
-                search: `?${queryString}`,
-              })
-            }}
-          >
-            Select Lab
-          </button>
-          {/* </Link> */}
-        </div>
-      </div>
-    )
-  })
+      )
+    })
 
-  const largeLabs = labArr.map((lab, index) => {
     return (
-      <div
-        key={lab._id}
-        className={`grid grid-cols-chooseLab5 p-3  ${
-          index % 2 != 0 ? "bg-secondary-withOpacity" : "bg-transparent "
-        }
-      `}
-      >
-        <div className="grid grid-cols-chooseLab2">
-          <p className="font-medium col-span-2 text-xl">{lab.name}</p>
-          <div className="flex gap-[3rem] text-[0.85rem] col-span-2 my-1">
-            {/* <div className="flex items-center gap-1">
-              <Icon icon={"mdi:badge-outline"} color={"#E97F0E"} />
-              {lab.basic.certificate}
-            </div> */}
-            {/* <div className="flex items-center gap-1">
-              <Icon icon={"simple-line-icons:calender"} color={"#E97F0E"} />
-              <span className="font-bold">Timings:</span> {lab.basic.timings}
-            </div> */}
-          </div>
-          <div className="flex items-center gap-1 col-span-2">
-            <Icon
-              icon={"material-symbols:location-on-outline"}
-              color={"#E97F0E"}
-            />
-            <p className="text-[0.85rem] ">
-              {lab.address.address}, {lab.address.city} - {lab.address.zipcode}
-            </p>
-          </div>
-        </div>
-        <h1 className="justify-self-center self-center">{`${maxProcessingTime} hours`}</h1>
-        {/* <h1 className="justify-self-center self-center">{lab.distance} km</h1> */}
-        <h1 className="justify-self-center self-center text-xl">
-          ₹
-          {lab.tests.reduce((acc, curr) => {
-            return acc + curr.price
-          }, 0)}
-        </h1>
-        <div className="self-center justify-self-center">
-          {/* <Link to="/afterLab"> */}
-          <button
-            className="text-white  bg-primary rounded  px-4 py-1 mt-2"
-            onClick={() => {
-              const queryParams = {
-                lab: lab.name,
-                tests: tests,
-              }
-              const queryString = qs.stringify(queryParams)
-              navigate({
-                pathname: "/afterLab",
-                search: `?${queryString}`,
-              })
-            }}
-          >
-            Select Lab
-          </button>
-          {/* </Link> */}
-        </div>
+      <div>
+        <div className="lg:hidden">{labs}</div>
+        <div className="hidden lg:block">{largeLabs}</div>
       </div>
     )
-  })
-
-  return (
-    <div>
-      {labArr.length === 0 ? (
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-2xl font-bold">No Labs Found</h1>
-          <p className="text-lg">Please try again later</p>
-        </div>
-      ) : (
-        <div>
-          <div className="lg:hidden">{labs}</div>
-          <div className="hidden lg:block">{largeLabs}</div>
-        </div>
-      )}
-    </div>
-  )
+  } else {
+    console.log("returning no labs")
+    return (
+      <div className="flex items-center h-[50vh] justify-center text-2xl font-semibold">
+        <h1>No Labs Found</h1>
+      </div>
+    )
+  }
 }
