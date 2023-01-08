e8d7208b8268cc55c717ee7c7374238a0972ef79
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2023-01-08 19:20:12 +0000

    added: ssl certificates

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 import express from "express";
-const app = express();
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
 
 app.use(express.static('dist'));
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+// Listen both http & https ports
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+  key: fs.readFileSync('./ssl/privkey.pem'),
+  cert: fs.readFileSync('./ssl/fullchain.pem'),
+}, app);
+
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
 });
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+});
+
